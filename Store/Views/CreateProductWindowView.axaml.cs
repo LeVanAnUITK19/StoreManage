564acab847b757f973dd19769db792bb7890e5ab
LeVanAn <levanan1902006@nan1902006gmail.06gmailcom>
2025-10-19 12:51:16 +0700

    update

---
+++ Store/Views/CreateProductWindowView.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Store.ViewModels;
+using Store.Views;
+
+
+namespace Store;
+
+public partial class CreateProductWindowView : Window
+{
+    public CreateProductWindowView()
+    {
+        InitializeComponent();
+        DataContext = new CreateProductWindowViewModel();
+    }
+}
--- Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net9.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fcecf6e18211c20d3d891df091f80377e496644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f1c50e38f563f44feaa16df3ceee5ba454ef768")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
